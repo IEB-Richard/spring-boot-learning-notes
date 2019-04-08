c88c980a060d142d0ae4d32fbeccf18309259048
xiaoqiangliu <xiaoqiang.liu@yahoo.com>
2019-04-08 20:48:12 +0800

    basic profile settings

--- chapter3-spring-profile/src/main/java/com/springboot/chapter3/config/AppConfig.java
+++ chapter3-spring-profile/src/main/java/com/springboot/chapter3/config/AppConfig.java
@@ -1,8 +1,15 @@
 package com.springboot.chapter3.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Configuration
@@ -12,4 +19,39 @@ import org.springframework.stereotype.Service;
 @ComponentScan(basePackages = "com.springboot.chapter3.*", 
 excludeFilters= {@Filter(classes= {Service.class})})
 public class AppConfig {
+	
+	@Bean(name = "dataSource", destroyMethod = "close")
+	@Profile("dev")
+	public DataSource getDevDataSource() {
+		Properties props = new Properties();
+		props.setProperty("driver", "com.mysql.jdbc.Driver");
+		props.setProperty("url", "jdbc:mysql://localhost:3306/dev_spring_boot");
+		props.setProperty("username", "root");
+		props.setProperty("password", "123456");
+		DataSource dataSource = null;
+		try {
+			dataSource = BasicDataSourceFactory.createDataSource(props);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dataSource;
+	}
+	
+	@Bean(name = "dataSource", destroyMethod = "close")
+	@Profile("test")
+	public DataSource getTestDataSource() {
+		Properties props = new Properties();
+		props.setProperty("driver", "com.mysql.jdbc.Driver");
+		props.setProperty("url", "jdbc:mysql://localhost:3306/test_spring_boot");
+		props.setProperty("username", "root");
+		props.setProperty("password", "123456");
+		DataSource dataSource = null;
+		try {
+			dataSource = BasicDataSourceFactory.createDataSource(props);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dataSource;
+	}	
+	
 }
