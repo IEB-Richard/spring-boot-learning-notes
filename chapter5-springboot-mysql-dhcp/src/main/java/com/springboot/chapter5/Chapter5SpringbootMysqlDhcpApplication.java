beeda9f897ab36f392364651b6f380509796ba97
xiaoqiangliu <xiaoqiang.liu@yahoo.com>
2019-04-13 18:06:11 +0800

    demo project for springboot datasource

---
+++ chapter5-springboot-mysql-dhcp/src/main/java/com/springboot/chapter5/Chapter5SpringbootMysqlDhcpApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.chapter5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter5SpringbootMysqlDhcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter5SpringbootMysqlDhcpApplication.class, args);
+	}
+
+}
