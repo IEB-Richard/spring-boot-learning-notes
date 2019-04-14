dfc088c514e0dd1bfebd88e4ffbbff58928d4ad5
xiaoqiangliu <xiaoqiang.liu@yahoo.com>
2019-04-14 08:16:45 +0800

    add one basic demo project of H2, only contains the basic spring boot configurations

---
+++ chapter5-springboot-h2/src/main/java/com/springboot/chapter5/Chapter5SpringbootH2Application.java
@@ -0,0 +1,13 @@
+package com.springboot.chapter5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter5SpringbootH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter5SpringbootH2Application.class, args);
+	}
+
+}
